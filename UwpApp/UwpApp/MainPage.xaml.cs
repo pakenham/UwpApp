dc2e507481547edc36c1ae4b6c5a3ec073bdcfde
Pakenham <pakenham98@hotmail.com>
2017-08-25 15:15:22 +0700

    completely program commit

--- UwpApp/UwpApp/MainPage.xaml.cs
+++ UwpApp/UwpApp/MainPage.xaml.cs
@@ -22,70 +22,185 @@ namespace UwpApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        // creating variables
+        string op; 
+        double op1, op2, result ,r, opp ;
+
         public MainPage()
         {
             this.InitializeComponent();
-        }
-
-        private void num7_Click(object sender, RoutedEventArgs e)
-        {
-            //textbox1.Text += 7.ToString();
-            textbox1.Text += num7.Content;
-
-        }
-
-        private void num8_Click(object sender, RoutedEventArgs e)
-        {
-            textbox1.Text += num8.Content;
-        }
-
-        private void num9_Click(object sender, RoutedEventArgs e)
-        {
-            textbox1.Text += num9.Content;
-        }
-
-        private void num4_Click(object sender, RoutedEventArgs e)
-        {
-            textbox1.Text += num4.Content;
-        }
-
-        private void num5_Click(object sender, RoutedEventArgs e)
-        {
-            textbox1.Text += num5.Content;
-        }
-
-        private void num6_Click(object sender, RoutedEventArgs e)
-        {
-            textbox1.Text += num6.Content;
-        }
+        } // constructor
 
         private void num1_Click(object sender, RoutedEventArgs e)
         {
-            textbox1.Text += num1.Content;
+                textbox1.Text += "1";
         }
 
         private void num2_Click(object sender, RoutedEventArgs e)
         {
-            textbox1.Text += num2.Content;
+            textbox1.Text += "2";
         }
 
         private void num3_Click(object sender, RoutedEventArgs e)
         {
-            textbox1.Text += num3.Content;
+            textbox1.Text += "3";
+        }
+
+        private void num4_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text += "4";
+        }
+
+        private void num5_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text += "5";
+        }
+
+        private void num6_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text += "6";
+        }
+
+        private void num7_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text += "7";
+        }
+
+        private void num8_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text += "8";
+        }
+
+        private void num9_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text += "9";
         }
 
         private void num0_Click(object sender, RoutedEventArgs e)
         {
-            textbox1.Text += num0.Content;
+            textbox1.Text += "0";
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        /*private async void Button_Click(object sender, RoutedEventArgs e)
         {
             MediaElement mediaElement = new MediaElement();
             var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
-            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Teacher, I want to go home.");
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("I want to go home.");
             mediaElement.SetSource(stream, stream.ContentType);
             mediaElement.Play();
+        }*/
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text = string.Empty;
         }
+
+        private void plusbutton_Click(object sender, RoutedEventArgs e)
+        {
+            op1 = Convert.ToDouble(textbox1.Text);
+            op = "+";
+            textbox1.Text = " ";
+            /*if ()
+            {
+                myEqualMethod();
+            }*/
+     
+        }
+
+        private void equalbutton_Click(object sender, RoutedEventArgs e)
+        {
+            myEqualMethod();
+        }
+
+        private void myEqualMethod()
+        {
+            if(opp == 2)
+            {
+                textbox1.Text = Convert.ToString(r);
+                opp = 0;
+            }
+            op2 = Convert.ToDouble(textbox1.Text);  // convert string to double for calculating 
+              
+
+             switch (op)
+                {
+                    case "+":
+                        result = op1 + op2;
+                        textbox1.Text = Convert.ToString(result); // convert double to string for display
+                        break;
+                    case "-":
+                        result = op1 - op2;
+                        textbox1.Text = Convert.ToString(result);
+                        break;
+                    case "*":
+                        result = op1 * op2;
+                        textbox1.Text = Convert.ToString(result); // convert double to string for display
+                        break;
+                    case "/":
+                        if (op2 == 0)
+                        {
+                            textbox1.Text = "ERROR";
+                            break;
+                        }
+                        else
+                        {
+                            result = op1 / op2;
+                            textbox1.Text = Convert.ToString(result);
+                            break;
+                        }
+                    /*case "%":
+                        result = op2 * (op1 / 100);
+                        textbox1.Text = Convert.ToString(result);
+                        break;*/
+
+            }
+                      
+        }
+
+        private void minusbutton_Click(object sender, RoutedEventArgs e)
+        {
+            op1 = Convert.ToDouble(textbox1.Text);
+            op = "-";
+            textbox1.Text = " ";
+        }
+
+        private void multiply_Click(object sender, RoutedEventArgs e)
+        {
+            op1 = Convert.ToDouble(textbox1.Text);
+            op = "*";
+            textbox1.Text = " ";
+        }
+
+        private void divide_Click(object sender, RoutedEventArgs e)
+        {
+            op1 = Convert.ToDouble(textbox1.Text);
+            op = "/";
+            textbox1.Text = " ";
+        }
+
+        private void dot_Click(object sender, RoutedEventArgs e)
+        {
+            if(textbox1.Text == ".") // check if dot is already present/not
+            {
+                textbox1.Text = textbox1.Text;
+            }
+            else
+            {
+                textbox1.Text += ".";
+            }         
+        }
+
+        private void percent_Click(object sender, RoutedEventArgs e)
+        {
+            op2 = Convert.ToDouble(textbox1.Text);
+            opp = 2;
+            r = op1 * (op2 / 100);
+
+            
+            textbox1.Text = " ";
+
+        }
+
+
     }
 }
